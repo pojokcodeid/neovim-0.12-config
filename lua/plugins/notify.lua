069f653f163d02273b5cdb0845261fae2f445ba0
Pojok Code <pojokcodeid@gmail.com>
2026-04-04 21:50:18 +0700

    fix: config nvim notify

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -4,16 +4,7 @@ vim.pack.add({
 	{ src = "https://github.com/MunifTanjim/nui.nvim" },
 	{ src = "https://github.com/folke/noice.nvim" },
 })
-require("notify").setup({
-	timeout = 3000,
-	max_height = function()
-		return math.floor(vim.o.lines * 0.75)
-	end,
-	max_width = function()
-		return math.floor(vim.o.columns * 0.4)
-	end,
-	render = "wrapped-compact",
-})
+
 require("noice").setup({
 	messages = {
 		enabled = false,
@@ -40,4 +31,17 @@ require("noice").setup({
 		},
 	},
 })
+
+local notify = require("notify")
+notify.setup({
+	timeout = 3000,
+	max_height = function()
+		return math.floor(vim.o.lines * 0.75)
+	end,
+	max_width = function()
+		return math.floor(vim.o.columns * 0.4)
+	end,
+	render = "wrapped-compact",
+})
+vim.notify = notify.notify
 --:
